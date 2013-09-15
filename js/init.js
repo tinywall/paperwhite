b4abd584a45d0586d6f386cfb26a7617173c12b0
Arun David <arundavid.info@gmail.com>
2013-09-15 07:55:31 +0530

    HTML5 tag and fix for IE

---
+++ js/init.js
@@ -0,0 +1,9 @@
+document.createElement('header');
+document.createElement('hgroup');
+document.createElement('nav');
+document.createElement('menu');
+document.createElement('main');
+document.createElement('section');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('footer');
